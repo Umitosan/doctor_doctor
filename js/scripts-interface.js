5353823286ea3d96528273026d4f01ccdb24af92
Dominic Brown <yeahdom@gmail.com>
2017-05-26 11:59:35 -0700

    add search input that returns a list of 10 doctors by condition

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,6 +1,11 @@
-var getDoctors = ('./../scripts.js').getDoctors;
+var getDoctors = require('./../js/scripts.js').getDoctors;
+
 
 $(document).ready(function(){
 
+  $('#search_btn').click(function() {
+    var condition = $('.row input').val();
+    getDoctors(condition);
+  });
 
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 var apiKey = require('./../.env').apiKey;
 
 exports.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=' + apiKey)
+   .then(function(results) {
+      console.log("result: ", results);
+      console.log("result.data.length ", results.data.length);
+      results.data.forEach(function(doctor){
+        $('#search_results').append("<h4> Name: " + doctor.profile.first_name + doctor.profile.last_name + "</h4>");
+      });
     })
    .fail(function(error){
       console.log("fail");
